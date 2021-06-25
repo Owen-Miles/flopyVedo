6acae85e6fbbbffafcb34dccb12a489fc4d2aaba
Owen MILES <owen.miles@list.lu>
2021-06-25 08:45:29 +0200

    saving to switch laptops

--- scratchScripts/normalDist.py
+++ scratchScripts/normalDist.py
@@ -3,15 +3,15 @@ import numpy as np
 
 ## The Normal Distribution ##
 mean, sigma = -3, 0.5 # mean and standard deviation
-s = 10**np.random.normal(mean, sigma, 4000)
+s = np.random.normal(mean, sigma, 4000)
 print(s[0:10])
 abs(mean - np.mean(s))
 abs(sigma - np.std(s, ddof=1))
 
-count, bins, ignored = plt.hist(s, 30, density=True)
-# plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
-#                np.exp( - (bins - mean)**2 / (2 * sigma**2) ),
-#          linewidth=2, color='r')
+count, bins, ignored = plt.hist(s, 60, density=True)
+plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
+               np.exp( - (bins - mean)**2 / (2 * sigma**2) ),
+         linewidth=2, color='r')
 
 plt.show()
 
